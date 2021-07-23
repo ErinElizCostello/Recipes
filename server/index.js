67ba62f957d8aba19a053532724a02d8a0944870
Erin <erin.eliz.costello@gmail.com>
2021-07-23 17:39:23 -0600

    setting up connection info

--- server/index.js
+++ server/index.js
@@ -20,10 +20,12 @@ app.use(cors(
 }
 ));
 
-const host = process.env.DB_HOST || 'localhost';
-const user = process.env.DB_USER || 'root';
-const password = process.env.DB_PASS || 'admin';
-const database = process.env.DB_DATABASE || 'recipes';
+const host = process.env.DB_HOST 
+const user = process.env.DB_USER 
+const password = process.env.DB_PASS 
+const database = process.env.DB_DATABASE 
+
+
 
 db = mysql.createConnection({
   host, user, password, database,
